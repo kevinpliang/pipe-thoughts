ffd44044ed9123aaeacee540fc389d6f7269b8b8
Kevin Liang <kevinpliang@gmail.com>
2021-03-13 19:10:55 -0500

    posts database

--- blog/views.py
+++ blog/views.py
@@ -1,24 +1,10 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-
-posts = [
-    {
-        'author': 'Kevin',
-        'title': 'Blog Post 1',
-        'content' : 'First post content',
-        'date_posted' : 'March 13, 2021'
-    },
-    {
-        'author': 'Rahil',
-        'title': 'Blog Post 2',
-        'content' : 'Second post content',
-        'date_posted' : 'March 14, 2021'
-    }
-]
+from .models import Post
 
 def home(request):
     context = {
-        'posts':posts
+        'posts':Post.objects.all()
     }
     return render(request, 'blog/home.html', context)
 
